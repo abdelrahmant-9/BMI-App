d61f8b817f58534ec322e5e9861390c30a960f42
Abdelrahman <abdot9674@gmail.com>
2026-02-22 02:45:18 +0200

    Add http package and refactor OnBoarding widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:bmi/ResultPage.dart';
+import 'package:bmi/onboarding_screen.dart';
 import 'package:flutter/material.dart';
-import 'Calculate_BMI.dart';
-import 'core/theme/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,99 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnBoarding(),
-    );
-  }
-}
-
-class OnBoarding extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Image.asset('assets/images/first_img.png',
-              fit: BoxFit.contain,
-              ),
-            ),
-          ),
-
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(32),
-              topRight: Radius.circular(32),
-            ),
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(24, 28, 24, 32),
-              color: AppColors.primary,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Know Your Body Better,\nGet Your BMI Score in Less Than a Minute!',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 22,
-                      height: 1.4,
-                      color: AppColors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  Text(
-                    'It takes just 30 seconds – and your health is worth it!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 15,
-                      height: 1.5,
-                      color: AppColors.greyWhite,
-                    ),
-                  ),
-
-                  Divider(
-                    color: AppColors.greyWhite,
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  SizedBox(
-                    width: double.infinity,
-                    height: 48,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const BmiScreen()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.darkPurple,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        elevation: 0,
-                      ),
-                      child: const Text(
-                        "Get Start",
-                        style: TextStyle(
-                          color: AppColors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+      home: OnboardingScreen(),
     );
   }
 }
